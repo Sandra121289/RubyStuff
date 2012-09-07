fb782d6a4f6884fc928216049b7878cfc6898e37
Sandra121289 <sandra.unger121289@gmail.com>
2012-09-08 01:42:12 +0200

    migration fix

--- db/migrate/20120907150600_create_betraeges.rb
+++ db/migrate/20120907150600_create_betraeges.rb
@@ -1,9 +1,10 @@
 class CreateBetraeges < ActiveRecord::Migration
   def change
     create_table :betraeges do |t|
+      t.string :vorname
       t.string :name
       t.decimal :betrag, :precision => 7, :scale => 2
-
+      t.string :email
       t.timestamps
     end
   end
