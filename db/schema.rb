71b67c66c2f777db97ec0a5190ce3e2e00f7ff66
Sandra121289 <sandra.unger121289@gmail.com>
2012-09-08 02:09:30 +0200

    migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120907234653) do
+ActiveRecord::Schema.define(:version => 20120908000645) do
 
   create_table "betraeges", :force => true do |t|
     t.string   "vorname"
